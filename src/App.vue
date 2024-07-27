6c40ef9e0eddf9e50b55204b4056c50afd7c7ab3
YJ <zerosix1017@gmail.com>
2024-07-27 21:44:23 +0900

    practice #1

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,52 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="menu">
+    <a v-for="(menu, i) in menus" :key="i">{{menu}}</a>
+  </div>
+  <br/>
+  <div v-for="(item, i) in products" :key="i" class="product-item">
+    <h4>{{products[i]}}</h4>
+    <p>100만원</p>
+    <button @click="increase(i)">신고</button>
+    <span>신고수 : {{reports[i]}}</span>
+  </div>
 </template>
 
+<script>
+export default {
+  name : 'App',
+  data(){
+    return {
+      products: ['A', 'B', 'C'],
+      prices: [100, 200, 300],
+      menus: ['Home', 'Shop', 'About'],
+      reports : [0, 0, 0]
+    }
+  },
+
+  methods : {
+    increase(i) {
+      this.reports[i] += 1;
+    }
+  }
+}
+
+</script>
+
 <style scoped>
+.product-item {
+  margin-bottom: 16px; /* 원하는 간격으로 조정 */
+}
+
+.menu {
+  background : darkslateblue;
+  padding : 15px;
+  border-radius : 5px;
+}
+.menu a {
+  color : white;
+  padding : 10px;
+}
+
 header {
   line-height: 1.5;
 }
